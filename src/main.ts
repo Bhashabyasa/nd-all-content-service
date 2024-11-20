0ac4e5233632201b857b58fca84cfc10b851ecb3
SethuK-ND <sethu@navadhiti.com>
2024-11-20 15:52:13 +0530

    add port in envfile

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3008, '0.0.0.0');
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 AppClusterService.clusterize(bootstrap);
